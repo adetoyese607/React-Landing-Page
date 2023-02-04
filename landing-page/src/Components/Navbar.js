082f70669ba1de570fbb940c989d5d4f7f516273
ADETOYESE Samson <adetoyesesamson@gmail.com>
2023-02-04 15:43:24 +0100

    push

---
+++ landing-page/src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Logo from "../Assets/footer-logo.png"
+
+const Navbar = () => {
+  return (
+    <div>
+      Navbar 
+    </div>
+  )
+}
+
+export default Navbar;
