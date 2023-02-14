12e7b63f2ee2a9b76d9fc14e238072e9e26968e1
ADETOYESE Samson <adetoyesesamson@gmail.com>
2023-02-14 09:30:35 +0100

    day13

--- landing-page/src/Components/About.js
+++ landing-page/src/Components/About.js
@@ -1,9 +1,30 @@
 import React from 'react'
-
+import AboutBackgroundImage from "../Assets/image.jpeg"
+import { BsFillPlusCircleFill } from 'react-icons/bs'
 const About = () => {
   return (
-    <div>
-      
+    <div className='about-section-container'>
+    <div className='about-background-image-container'>
+        {/* <img src={AboutBackgroundImage} alt=""/> */}
+        </div>  
+        <div className='about-section-image-container'>
+        <img src={AboutBackgroundImage} alt=""/>
+        </div>
+        <div className='about-section-text-container'>
+            <p className='primary-subheading'>About</p>
+            <h1 className='primary-heading'>
+                Food is An Important Part Of A Balanced Diet</h1>
+                <p className='primary-text'>
+                    Lorem ipsum dolor sit amet, consectetur
+                     adipisicing elit. Recusandae officiis fugiata? 
+                </p>
+                <div className='about-buttons-container'>
+                  <button className='secondary-button'>Learn More</button>
+                  <button className='watch-video-button'>
+                     <BsFillPlusCircleFill/> Watch Video
+                      </button>
+                </div>
+        </div>
     </div>
   )
 }
